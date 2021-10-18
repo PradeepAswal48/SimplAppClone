393891a3bd9697ed2821b181bb77c7a3d5ae39ec
Pradeep Aswal <35682978+PradeepAswal48@users.noreply.github.com>
2021-10-18 10:08:32 +0530

    navigator implementation started

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,3 +1,45 @@
 export default function Navigator() {
-  return <div>navigate from here</div>;
+  return (
+    <>
+      <div className="item selected">
+        <span className="item-icon">
+          <img src={require("../assets/icons/home.png")} alt="account logo" />
+        </span>
+        <span className="item-text">Home</span>
+      </div>
+      <div className="item">
+        <span className="item-icon">
+          <img
+            src={require("../assets/icons/Passbook.png")}
+            alt="account logo"
+          />
+        </span>
+        <span className="item-text">Passbook</span>
+      </div>
+      <div className="item">
+        <span className="item-icon">
+          <img
+            src={require("../assets/icons/Billbox.png")}
+            alt="account logo"
+          />
+        </span>
+        <span className="item-text">Billbox</span>
+      </div>
+      <div className="item">
+        <span className="item-icon">
+          <img
+            src={require("../assets/icons/Profile.png")}
+            alt="account logo"
+          />
+        </span>
+        <span className="item-text">Profile</span>
+      </div>
+      <div className="item">
+        <span className="item-icon">
+          <img src={require("../assets/icons/Group.png")} alt="account logo" />
+        </span>
+        <span className="item-text">More</span>
+      </div>
+    </>
+  );
 }
